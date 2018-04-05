8748430e9162bd47c31e56fdd100e6fdff535f32
mbankston <mbankston@mitre.org>
2018-04-05 12:54:13 -0600

    Renamed Django sample project. Added templates to testApp.

---
+++ Python/Django/learning_site/testApp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+# These methods get a HttpRequest object and return an HttpResponse object
+
+
+def index(request):
+    context = {}
+
+    # render(request, url, context) is a shortcut for doing this...
+    # template = loader.get_template('testApp/index.html')
+    # return HttpResponse(template.render(context, request))
+
+    return render(request, 'testApp/index.html', context)
+    # return HttpResponse('This is an HttpResponse.')
