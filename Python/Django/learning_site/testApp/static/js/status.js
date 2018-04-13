5c9eaaa07b283a4ba596a8d7bbe7defe60e5d93b
mbankston <mbankston@mitre.org>
2018-04-13 14:14:44 -0600

    Added Chart.js example to status page.

--- Python/Django/learning_site/testApp/static/js/status.js
+++ Python/Django/learning_site/testApp/static/js/status.js
@@ -5,13 +5,13 @@
 
   // Use $ -> to ensure document is ready.
   $(function() {
-    var webSocketBridge;
+    var ctx, myChart, webSocketBridge;
     $('#status_div').append($('<p>').attr('id', 'message_display').append('No message.'));
     webSocketBridge = new channels.WebSocketBridge();
     webSocketBridge.connect('/ws/');
     // Setup a listener callback function for anything that gets sent on this web socket
     webSocketBridge.listen(function(msg_ob, stream) {
-      $('#message_display').html('<b>' + msg_ob.value + '</b>');
+      $('#message_display').text(msg_ob.value);
     });
     // Setup a listener callback function for a specific stream channel (called message_stream) 
     // on this web socket
@@ -25,6 +25,46 @@
       webSocketBridge.stream('message_stream').send({
         message: "Example streaming text."
       });
+    });
+    ctx = $('#chartjs_canvas')[0].getContext('2d');
+    myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12,
+          19,
+          3,
+          5,
+          2,
+          3],
+            backgroundColor: ['rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'],
+            borderColor: ['rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'],
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        scales: {
+          yAxes: {
+            ticks: {
+              beginAtZero: true
+            }
+          }
+        }
+      }
     });
   });
 
