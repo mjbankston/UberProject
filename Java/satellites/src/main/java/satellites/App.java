92060d71ea0d06b8f566eb78971e22d19f298a83
mbankston <mbankston@mitre.org>
2018-04-23 10:45:02 -0600

    Many Orekit usage examples worked through.

--- Java/satellites/src/main/java/satellites/App.java
+++ Java/satellites/src/main/java/satellites/App.java
@@ -1,7 +1,16 @@
 package satellites;
 
 import java.io.File;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 
+import org.hipparchus.geometry.euclidean.threed.Vector3D;
+import org.hipparchus.util.FastMath;
+import org.hipparchus.util.MathUtils;
+import org.orekit.bodies.BodyShape;
+import org.orekit.bodies.GeodeticPoint;
+import org.orekit.bodies.OneAxisEllipsoid;
 import org.orekit.data.DataProvidersManager;
 import org.orekit.data.DirectoryCrawler;
 import org.orekit.errors.OrekitException;
@@ -9,20 +18,145 @@ import org.orekit.frames.Frame;
 import org.orekit.frames.FramesFactory;
 import org.orekit.orbits.KeplerianOrbit;
 import org.orekit.orbits.Orbit;
+import org.orekit.orbits.OrbitType;
 import org.orekit.orbits.PositionAngle;
 import org.orekit.propagation.SpacecraftState;
 import org.orekit.propagation.analytical.KeplerianPropagator;
+import org.orekit.propagation.analytical.tle.TLE;
+import org.orekit.propagation.analytical.tle.TLEPropagator;
 import org.orekit.time.AbsoluteDate;
 import org.orekit.time.TimeScale;
 import org.orekit.time.TimeScalesFactory;
+import org.orekit.utils.Constants;
+import org.orekit.utils.PVCoordinates;
 
 public class App {
-    public static void main(String[] args) throws OrekitException {
 
+    // The target coordinates (Colorado Springs)
+    public static double targetLatDegrees = 38.8;
+    public static double targetLonDegrees = -104.8;
+    public static double targetAltMeters = 1750.0;
+
+    // Monitoring station 1 coordinates (San Antonio)
+    public static double mon1LatDegrees = 29.4;
+    public static double mon1LonDegrees = -98.5;
+    public static double mon1AltMeters = 198.0;
+
+    // Monitoring station 2 coordinates (San Diego)
+    public static double mon2LatDegrees = 32.7;
+    public static double mon2LonDegrees = -117.15;
+    public static double mon2AltMeters = 19.0;
+
+    // SES-3
+    public static String tle1Line1 = "1 37748U 11035A   18109.87218494 -.00000112 +00000-0 +00000-0 0  9996";
+    public static String tle1Line2 = "2 37748 000.0410 067.9524 0001724 308.8489 042.1154 01.00271942024721";
+
+    // G-16
+    public static String tle2Line1 = "1 29236U 06023A   18110.48934799 -.00000139  00000-0  00000+0 0  9990";
+    public static String tle2Line2 = "2 29236   0.0143 223.8848 0002723 162.4695 259.3224  1.00270133 43386";
+
+    public static void main(String[] args) throws OrekitException {
+        // Orekit needs to be initialized with physical data files.
+        // These were downloaded from the Orekit website as the "example quick start" data files.
+        // Not sure if there are other physical data files that would be better used.
+        initOreKit();
+
+        // Get the current time as UTC
+        Date now = Calendar.getInstance(TimeZone.getTimeZone("UTC")).getTime();
+
+        System.out.println("Current date is " + now.toString() + '\n');
+
+        // Orekit uses the GeodeticPoint class to represent a lat/lon/alt location on a body.
+        // Create three ground stations: target, monitoring station 1, and monitoring station 2
+        GeodeticPoint target = new GeodeticPoint(Math.toRadians(targetLatDegrees), Math.toRadians(targetLonDegrees),
+                targetAltMeters);
+        System.out.println("Target location is at " + target.getLatitude() + " deg latitude (" + target.getLatitude()
+                + " rad), " + targetLonDegrees + " deg longitude (" + target.getLongitude() + " rad), altitude "
+                + target.getAltitude() + " meters.");
+        GeodeticPoint mon1 = new GeodeticPoint(Math.toRadians(mon1LatDegrees), Math.toRadians(mon1LonDegrees),
+                mon1AltMeters);
+        System.out.println("Monitor 1 location is at " + mon1.getLatitude() + " deg latitude (" + mon1.getLatitude()
+                + " rad), " + mon1LonDegrees + " deg longitude (" + mon1.getLongitude() + " rad), altitude "
+                + mon1.getAltitude() + " meters.");
+        GeodeticPoint mon2 = new GeodeticPoint(Math.toRadians(mon2LatDegrees), Math.toRadians(mon2LonDegrees),
+                mon2AltMeters);
+        System.out.println("Monitor 2 location is at " + mon2.getLatitude() + " deg latitude (" + mon2.getLatitude()
+                + " rad), " + mon2LonDegrees + " deg longitude (" + mon2.getLongitude() + " rad), altitude "
+                + mon2.getAltitude() + " meters.");
+
+        // Propagate the two given satellite TLE's to the current time using the Orekit TLE SGP4/SDP4 propagator.
+        // This currenty does not check TLE epoch vs current time to determine how realistic this propagation is.
+        PVCoordinates sat1Coor = propagateTLE(tle1Line1, tle1Line2, now);
+        System.out.println("\nSatellite 1 TLE:\n" + tle1Line1 + '\n' + tle1Line2 + '\n');
+        System.out.println("Propagated to " + sat1Coor + '\n');
+        PVCoordinates sat2Coor = propagateTLE(tle2Line1, tle2Line2, now);
+        System.out.println("Satellite 2 TLE:\n" + tle2Line1 + '\n' + tle2Line2 + '\n');
+        System.out.println("Propagated to " + sat2Coor + '\n');
+
+        // Create a BodyShape object of Earth in an unintutive way for measuring/manipulating coordinates and distances.
+        // Uses WGS84 data and IERS_1996 data to build the shape. Supposedly, this is compatible with SGP4/SDP4 TLE propagation.
+        BodyShape earth = new OneAxisEllipsoid(Constants.WGS84_EARTH_EQUATORIAL_RADIUS,
+                Constants.WGS84_EARTH_FLATTENING, FramesFactory.getTEME());
+
+        // Take the lat/lon/height of ground stations and convert to 3D vectors in the same coordinate frame as the satellite location propagation.
+        Vector3D targetPosition = earth.transform(target);
+        Vector3D mon1Position = earth.transform(mon1);
+        Vector3D mon2Position = earth.transform(mon2);
+
+        // Determine all combinations of distance from ground to satellite.
+        double dt1 = Vector3D.distance(targetPosition, sat1Coor.getPosition());
+        double dt2 = Vector3D.distance(targetPosition, sat2Coor.getPosition());
+
+        double dm11 = Vector3D.distance(mon1Position, sat1Coor.getPosition());
+        double dm12 = Vector3D.distance(mon1Position, sat2Coor.getPosition());
+
+        double dm21 = Vector3D.distance(mon2Position, sat1Coor.getPosition());
+        double dm22 = Vector3D.distance(mon2Position, sat2Coor.getPosition());
+
+        System.out.println("Distance from target to satellite 1:               " + dt1 + " meters.");
+        System.out.println("Distance from target to satellite 1:               " + dt2 + " meters.");
+        System.out.println("Distance from monitoring station 1 to satellite 1: " + dm11 + " meters.");
+        System.out.println("Distance from monitoring station 1 to satellite 1: " + dm12 + " meters.");
+        System.out.println("Distance from monitoring station 2 to satellite 1: " + dm21 + " meters.");
+        System.out.println("Distance from monitoring station 2 to satellite 1: " + dm22 + " meters.");
+
+    }
+
+    private static void initOreKit() throws OrekitException {
         File orekitData = new File("C:\\orekit-data");
         DataProvidersManager manager = DataProvidersManager.getInstance();
         manager.addProvider(new DirectoryCrawler(orekitData));
+    }
 
+    public static PVCoordinates propagateTLE(String tleLine1, String tleLine2, Date date) throws OrekitException {
+        TLE tle = buildTLEFromLines(tleLine1, tleLine2);
+        TLEPropagator tleProp = TLEPropagator.selectExtrapolator(tle);
+        SpacecraftState state = tleProp.propagate(new AbsoluteDate(date, TimeScalesFactory.getUTC()));
+        return state.getPVCoordinates();
+    }
+
+    public static TLE buildTLEFromLines(String tleLine1, String tleLine2) throws OrekitException {
+        // Always test if TLE is good first
+        if (!TLE.isFormatOK(tleLine1, tleLine2)) {
+            throw new RuntimeException("Bad TLE format!");
+        }
+        return new TLE(tleLine1, tleLine2);
+    }
+
+    public static TLE updateTLEFromKeplarianOrbit(TLE oldTLE, Orbit orbit) {
+        final KeplerianOrbit kep = (KeplerianOrbit) OrbitType.KEPLERIAN.convertType(orbit);
+        return new TLE(oldTLE.getSatelliteNumber(), oldTLE.getClassification(), oldTLE.getLaunchYear(),
+                oldTLE.getLaunchNumber(), oldTLE.getLaunchPiece(), TLE.DEFAULT, oldTLE.getElementNumber(),
+                kep.getDate(), kep.getKeplerianMeanMotion(), 0.0, 0.0, kep.getE(),
+                MathUtils.normalizeAngle(kep.getI(), FastMath.PI),
+                MathUtils.normalizeAngle(kep.getPerigeeArgument(), FastMath.PI),
+                MathUtils.normalizeAngle(kep.getRightAscensionOfAscendingNode(), FastMath.PI),
+                MathUtils.normalizeAngle(kep.getMeanAnomaly(), FastMath.PI), oldTLE.getRevolutionNumberAtEpoch(),
+                oldTLE.getBStar());
+    }
+
+    // Example on Orekit tutorial
+    public static void test1() throws OrekitException {
         Frame inertialFrame = FramesFactory.getEME2000();
 
         TimeScale utc = TimeScalesFactory.getUTC();
@@ -54,5 +188,6 @@ public class App {
             System.out.println(" time : " + currentState.getDate());
             System.out.println(" " + currentState.getOrbit());
         }
+
     }
 }
